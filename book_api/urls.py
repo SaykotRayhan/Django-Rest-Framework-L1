d3b845bdf67f75916856cff53a96a83a0cb878ef
Rayhan <saykotrayhan199@gmail.com>
2022-05-16 20:52:25 +0600

    First API completed

---
+++ book_api/urls.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.urls import path
+# from book_api.views import book_list, book_create, book
+from book_api.views import BookList, BookCreate, BookDetails
+
+urlpatterns = [
+    path('', BookCreate.as_view()),
+    path('list/', BookList.as_view()),
+    path('<int:pk>', BookDetails.as_view()),
+]
+
+# urlpatterns = [
+#     path('', book_create),
+#     path('list/', book_list),
+#     path('<int:pk>', book),
+# ]
