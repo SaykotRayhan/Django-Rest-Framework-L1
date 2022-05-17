0922d14c4c1559962ef619699cf1fcc4151722d0
Rayhan <saykotrayhan199@gmail.com>
2022-05-17 21:45:03 +0600

    Implemented mixins

--- book_api/urls.py
+++ book_api/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path
 # from book_api.views import book_list, book_create, book
+from rest_framework.urlpatterns import format_suffix_patterns
+
 from book_api.views import BookList, BookCreate, BookDetails
 
 urlpatterns = [
@@ -8,7 +10,6 @@ urlpatterns = [
     path('list/', BookList.as_view()),
     path('<int:pk>', BookDetails.as_view()),
 ]
-
 # urlpatterns = [
 #     path('', book_create),
 #     path('list/', book_list),
--- book_api/views.py
+++ book_api/views.py
@@ -3,55 +3,90 @@ from django.http import JsonResponse
 from book_api.models import Book
 from book_api.serializer import BookSerializer
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status, generics
 from rest_framework.decorators import api_view
 from rest_framework.views import APIView
+from rest_framework import mixins
 
 
-#Class based view
-class BookList(APIView):
-    def get(self, request):
-        books = Book.objects.all()
-        serializer = BookSerializer(books, many=True)
-        return Response(serializer.data)
+# Class based view
+# class BookList(APIView):
+#     def get(self, request):
+#         books = Book.objects.all()
+#         serializer = BookSerializer(books, many=True)
+#         return Response(serializer.data)
+
+# Class based view with mixin
+class BookList(mixins.ListModelMixin,  generics.GenericAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
 
 
-class BookCreate(APIView):
-    def post(self, request):
-        serializer = BookSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        else:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+# class BookCreate(APIView):
+#     def post(self, request):
+#         serializer = BookSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         else:
+#             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+# Using mixin
+class BookCreate(mixins.CreateModelMixin, generics.GenericAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
 
 
-class BookDetails(APIView):
-    def get_book_by_pk(self, pk):
-        try:
-            return Book.objects.get(pk=pk)
-        except:
-            return Response({
-                'error': 'Books does not found'
-            }, status=status.HTTP_404_NOT_FOUND)
+# class BookDetails(APIView):
+#     def get_book_by_pk(self, pk):
+#         try:
+#             return Book.objects.get(pk=pk)
+#         except:
+#             return Response({
+#                 'error': 'Books does not found'
+#             }, status=status.HTTP_404_NOT_FOUND)
+#
+#     def get(self, request, pk):
+#         book = self.get_book_by_pk(pk)
+#         serializer = BookSerializer(book)
+#         return Response(serializer.data)
+#
+#     def put(self, request, pk):
+#         book = self.get_book_by_pk(pk)
+#         serializer = BookSerializer(book, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#
+#     def delete(self, request, pk):
+#         book = self.get_book_by_pk(pk)
+#         book.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
 
-    def get(self, request, pk):
-        book = self.get_book_by_pk(pk)
-        serializer = BookSerializer(book)
-        return Response(serializer.data)
+# using mixins
+class BookDetails(mixins.RetrieveModelMixin,
+                    mixins.UpdateModelMixin,
+                    mixins.DestroyModelMixin,
+                    generics.GenericAPIView):
 
-    def put(self, request, pk):
-        book = self.get_book_by_pk(pk)
-        serializer = BookSerializer(book, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
 
-    def delete(self, request, pk):
-        book = self.get_book_by_pk(pk)
-        book.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
 
 
 
